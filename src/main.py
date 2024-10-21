45348b063d5ad849c6c5b984c6a8830c35013c37
YumaIshigooka <yuma.ishigooka01@estudiant.upf.edu>
2024-10-21 16:06:59 +0200

    copied confidence interval function no se que hace

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
-print("Hello World")
+import numpy as np
+import scipy.stats
+
+# Function from:
+# https://stackoverflow.com/questions/15033511/compute-a-confidence-interval-from-sample-data
+def mean_confidence_interval(data, confidence=0.95):
+    a = 1.0 * np.array(data)
+    n = len(a)
+    m, se = np.mean(a), scipy.stats.sem(a)
+    h = se * scipy.stats.t.ppf((1 + confidence) / 2., n-1)
+    return m, m-h, m+h
