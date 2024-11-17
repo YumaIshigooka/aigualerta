f0ce63c6d3b63ae34a294c0a44dd0dc44e857e28
Adrià León <114679975+AdriaLeon@users.noreply.github.com>
2024-11-17 13:06:41 +0100

    AQ-103 user interface (#21)
    * column per hour and elimination of null rows added
    
    * title, and upload of the file to do the computations
    
    * cleaning of the data button (not needed in the final version) and expected columns added
    
    * main added, requirements.txt filled and ipynb moved
    
    * typo removed
    
    * conflicts solved
    
    * title, and upload of the file to do the computations
    
    * cleaning of the data button (not needed in the final version) and expected columns added
    
    * git rebase --continuemain added, requirements.txt filled and ipynb moved
    
    * typo removed
    
    * conflicts solved
    
    * Update requirements.txt
    
    * short names for columns added
    
    * main() --> load_page() && button to generate a dummy plot
    
    * additional page and hide button for the dump plot
    
    * main_page divided in main and utils && modularity added to the functions so they are easier to understand and reuse
    
    * tab system implemented, the programm should adapt to every tab<number>.py file non-repeated you add
    
    * typo fixed
    
    * setup_data && setup_input && updated requirements.txt
    
    * extra files removal && new tab 3 that loads the data from path
    
    * presentation page implement && load_image function added to utils.py
    
    * minor changes
    
    * new version of setup_input && setup data
    
    * session atr added && predict function now is called correctly
    
    * prediction added to the 'Data Upload' tab

---
+++ src/interface/tabs/tab0.py
@@ -0,0 +1,51 @@
+import utils
+import streamlit as st
+
+
+def load_page():
+
+    st.title("Aigualerta")
+    utils.load_image("aigualerta_logo.png", width=150, center=True)
+
+    st.subheader("Project Summary")
+    st.write("""
+    The goal of this website is to efficiently detect potential water leaks and predict future issues in pipelines.
+    By analyzing fluctuations in water flow, the system aims to identify pipes that may develop defects or other damages over time.
+    Additionally, this tool helps in monitoring and regulating excessive water consumption, whether due to human error or leaks, ensuring the optimal and sustainable use of this precious resource.
+
+    We have initially used the Aigües de Barcelona (AB) dataset to train our gradient boosting model and generate accurate predictions.
+
+    In the following sections of this website, you can upload your own .csv or .parquet file containing relevant data, and our system will help you make predictions about potential water leaks or abnormal water consumption patterns in your data.
+    """)
+
+    st.subheader("Authors")
+    st.write("""
+        Jinsong Liu
+              
+        Marc Gutierrez
+             
+        Yuma Ishigooka
+             
+        Adrià León
+             
+        Suleyman Hasanov
+    """)
+
+    st.subheader("Acknowledgements")
+
+    utils.load_image("upf_logo.png", width = 300)
+    st.write("""
+    To [Miquel Olvier](https://www.linkedin.com/in/miquel-oliver/) for his contributions and valuable insights into this project.
+    """)
+
+    utils.load_image("ab_logo.png", width = 350)
+    st.write("""
+             
+    To [Aigües de Barcelona](https://www.aiguesdebarcelona.cat/ca/web/guest/) for providing their support and sharing their data with us.
+    """)
+
+    st.subheader("Feedback")
+    st.write("""
+        If you have any feedback, please reach out to us at yuma.ishigooka01@estudiant.upf.edu.
+    """)
+
